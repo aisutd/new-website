2cbfebd30aee777af5735f75bcbb506dccdd3279
Joshua Baker <joshuakb2@gmail.com>
2019-04-02 21:27:10 -0500

    Created demo implementation of eventsLoader.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -10,6 +10,33 @@ else {
 }
 
 
-function init() {
-    
+async function init() {
+    let eventsFragment = loadEvents(createEventDiv);
+
+    let container = document.getElementById('events-list');
+
+    container.innerText = 'Downloading event list. Please wait...';
+
+    let fragment = await eventsFragment;
+
+    container.empty();
+    container.append(fragment);
+
+    console.log('Events loaded.');
+}
+
+function createEventDiv(event) {
+    let div = document.createElement('DIV');
+
+    div.className = 'event';
+    div.innerHTML = 
+        `What: ${event.summary}<br>` +
+        `When: ${event.start}<br>` +
+        `Where: ${event.location}<br>` +
+        `<br>` +
+        `Description:<br>` +
+        `${event.description}`
+        ;
+
+    return div;
 }
