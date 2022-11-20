fd34ab71d1d3127745149c0261e179b9d1be3c10
lokeshyerneni <lokeshyerneni0429@gmail.com>
2022-11-20 14:34:13 -0600

    Made changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
-import type { AppProps } from 'next/app';
-import 'tailwindcss/tailwind.css';
-import SiteLayout from '../components/common/SiteLayout';
-import '../styles/fonts.css';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-/**
- * A wrapper for the root website component.
- */
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SiteLayout>
-      <Component {...pageProps} />
-    </SiteLayout>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
--- pages/api/projects.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectDemo } from '../../lib/types';
-
-/**
- * Return all project demos.
- */
-export function getDemos(): ProjectDemo[] {
-  return [];
-}
