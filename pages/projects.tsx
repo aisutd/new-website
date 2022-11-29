f618fd644a928a5614da8aba519c251d8c5b9375
Ibad Momin <ibadalimomin@gmail.com>
2022-11-29 13:48:37 -0600

    comming soon box

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import React from 'react';
 import { getDemos } from './api/projects';
 import { ProjectDemo } from '../lib/types';
+import toroid from '../public/images/SuperToroid-2.png';
 
 interface DemoPageProps {
   demos: ProjectDemo[];
@@ -22,8 +23,15 @@ export default function ProjectsPage({ demos }: DemoPageProps) {
           content="An overview of all our AI/ML projects, including explanations and interactive demos."
         />
       </Head>
-      <main className="flex flex-col justify-center min-h-screen">
-        <div className="text-center font-bold">Coming soon</div>
+      {/* 1.add background gradient color 
+          2. create coming soon box/div and add shadow
+          3. insert cube and rectangle images*/}
+      <main className="flex flex-col justify-center min-h-screen bg-gradient-to-r from-blue-300 to-blue-200">
+        <div className="text-center font-bold w-9/12 h-5/6 p-10 rounded-3xl shadow-lg shadow-slate-500 m-auto mt-auto">
+          <p className="text-center pt-10 h-9/12">Comming Soon</p>
+          {/* <img src="images/SuperToroid-2.png" className="w-20" />
+          <img src="images/RoundCube-Blue-Glossy.png" className="w-20" /> */}
+        </div>
       </main>
     </div>
   );
