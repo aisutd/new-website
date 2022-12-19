208c77c8756a653d03e74d32e2d2e4af9170d908
Ibad Momin <ibadalimomin@gmail.com>
2022-12-19 16:46:52 -0600

    background color change

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import React from 'react';
 import { getDemos } from './api/projects';
 import { ProjectDemo } from '../lib/types';
-import toroid from '../public/images/SuperToroid-2.png';
 
 interface DemoPageProps {
   demos: ProjectDemo[];
@@ -17,22 +16,24 @@ export default function ProjectsPage({ demos }: DemoPageProps) {
     <div>
       <Head>
         <title>Projects &ndash; AIS</title>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />s
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <meta
           name="description"
           content="An overview of all our AI/ML projects, including explanations and interactive demos."
         />
       </Head>
-      <main className="flex flex-col justify-center min-h-screen bg-gradient-to-r from-blue-300 to-blue-200">
+      <main className="flex flex-col justify-center min-h-screen bg-gradient-to-r from-blue-100 to-blue-50">
         <div className="text-center font-bold w-9/12 h-full p-10 rounded-3xl shadow-lg shadow-slate-500 m-auto mt-auto">
-          <p className="text-center pt-10 h-9/12 leading-10 text-3xl">Coming Soon</p>
+          <p className="text-center pt-10 h-9/12 leading-10 text-3xl translate-y-20 text-gray-700 font-semibold">
+            Coming Soon
+          </p>
           <img
             src="images/RoundCube-Blue-Glossy.png"
-            className="w-30 float-right translate-x-20 translate-y-20 opacity-80"
+            className="w-30 float-right translate-x-20 translate-y-20 opacity-80 invisible lg:visible"
           />
           <img
             src="images/SuperToroid-2.png"
-            className="w-30 float-left -translate-x-3/4 -translate-y-12 opacity-90"
+            className="w-30 float-left -translate-x-3/4 -translate-y-12 opacity-90 invisible lg:visible"
           />
         </div>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     fontFamily: {
       sans: ['IBM Plex Sans', 'sans-serif'],
+      hind: ['Hind Siliguri'],
     },
   },
   variants: {
@@ -31,4 +32,3 @@ module.exports = {
   },
   plugins: [],
 };
-
